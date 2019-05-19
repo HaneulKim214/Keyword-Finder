4e0e0cc37e3756c9e137197ab87d7c8cc74d77e4
Haneul Kim <haneulkim214@gmail.com>
2019-05-18 22:41:14 -0400

    Moving it to my computer

--- main.py
+++ main.py
@@ -1,36 +1,102 @@
-from splinter import Browser
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+import matplotlib.pyplot as plt
+import nltk
 import os
 import pymongo
+import pandas as pd
+import re
+from splinter import Browser
+import time
 
-# use pymongo to store all info from LinkedIN
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.linkedin_db
+app = Flask(__name__)
 
-
-chrome_driver_path = os.path.abspath(r"C:\Users\haneu\Desktop\Data Analytics\6_mongo_webScrape\chromedriver.exe")
-executable_path = {'executable_path': chrome_driver_path}
-browser = Browser('chrome', **executable_path, headless=False)
-url = "https://www.glassdoor.ca/index.htm"
-browser.visit(url)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-def search_location():
-    print("Where do you want to find your job?")
-    job_location = input()
+
+@app.route("/scrape/<input>")
+def test(input):
+    title, loc = input.split("!")
+    print(title, f'location = {loc}')
+    # Call init function to open browser
+
+    # Initialize browser to use chrome and show its process.
+    executable_path = {'executable_path': "chromedriver.exe"}
+    browser = Browser('chrome', **executable_path, headless=False)
+    url = "https://www.glassdoor.ca/index.htm"
+    browser.visit(url)
+
+    # Find where we should fill job-title then fill it up
+    job_type = browser.find_by_id("KeywordSearch")
+    job_type.fill(title)
+
     location = browser.find_by_id("LocationSearch")
-    location.fill(job_location)
+    location.fill(loc)
+
     # Clicking button
     browser.find_by_id("HeroSearchButton").click()
 
+    # list to store scraped data
+    company = []
+    location = []
+    job_desc = []
+    position = []
+    
+    # scrape first page and append data into initial lists 
+    scrape_current_page(company, location, job_desc, position)
 
-search_location()
-def scrape_data():
+
+    # grab new html, grab page control elements
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
-    # loop through pages
-    pages = soup.find("div", class_="pagingControls").ul
-    print(pages)
+    result = soup.find("div", class_="pagingControls").ul
+    pages = result.find_all("li")
+
+    for page in pages:
+        # run if <a> exists since un-clickable do not have <a> skipping < and pg1
+        if page.a:
+            # within <a> tag click except next button         
+            if not page.find("li", class_="Next"):
+                try:
+                    # Click to goto next page, then scrape it.
+                    browser.click_link_by_href(page.a['href'])
+                    # --------- call scrape data function here ---------
+                    scrape_current_page()
+                except:
+                    print("This is the last page")
+
+
+def scrape_current_page(company, location, job_desc, position):
     
-scrape_data()
+    # Getting html of first page
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    jobs = soup.find_all("li", class_="jl")
+
+    for job in jobs:
+            
+        # Store all info into a list         
+        position.append(job.find("div", class_="jobTitle").a.text)
+        # ex: Tommy - Singapore
+        comp_loc = job.find("div", class_="empLoc").div.text
+        comp, loc = comp_loc.split("–")
+        # print(comp)
+        company.append(comp.strip())
+        location.append(loc.strip())
+        
+        browser.click_link_by_href(job.find("a", class_="jobLink")["href"])
+        
+        # ------------- Scrape Job descriptions within a page -----------
+        # job description is in another html, therefore retrieve it once again after
+        # clicking.
+        html = browser.html
+        soup = BeautifulSoup(html, "html.parser")
+        job_desc.append(soup.find("div", class_="desc").text)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,35 @@
+console.log("asd");
+// give reference to "Go Search" button
+var go_search = d3.select("#submit");
+
+
+go_search.on("click", function(){
+    d3.event.preventDefault();
+    
+    // grab input value and store into variable
+    var title = d3.select("#job-title").node().value;
+    var loc = d3.select("#job-loc").node().value;
+    
+    // refresh search bars
+    d3.select("#job-title").node().value = "";
+    d3.select("#job-loc").node().value = "";
+    console.log(title);
+    console.log(loc);
+
+
+    // Since HTTP only allow to send string we split title and location by "/" which
+    // we will split in python to separate two words.
+    var input = title +"!" + loc
+    console.log(input);
+   
+    // send it off to scrape function 
+    scrape(input);
+})
+
+function scrape(input){
+    // send input to python
+    var url = `/scrape/${input}`;
+    d3.json(url).then(function(data){
+        
+    });
+};
