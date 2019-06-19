2205a67596ece99de2c818e20399ff2ddee64668
haneul kim <haneulkim214@gmail.com>
2019-06-19 14:08:59 -0400

    gigignore file added

--- googleMapApi.py
+++ googleMapApi.py
@@ -1,14 +1,14 @@
 import requests
 import json
+import config
 
 def get_geocode(location, company):
-    api_key = "AIzaSyBPtKgw6FBdWbmvt1Og_nrDdcAe9eFGSQA"
     geocodes = []
     
     for i in range(len(company)):
         loc = location[i]
         comp = company[i]
-        url = f'https://maps.googleapis.com/maps/api/geocode/json?address={comp},{loc}&key={api_key}'
+        url = f'https://maps.googleapis.com/maps/api/geocode/json?address={comp},{loc}&key={config.api_key}'
         response = requests.get(url).json()
         # {lat, lng}
         geocodes.append(response["results"][0]["geometry"]["location"])
